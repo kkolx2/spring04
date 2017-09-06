010f426b9da3b8874c0e6ef80a0c748d29dbfc2a
java@guestBig <kkolx2@nate.com>
2017-09-06 16:19:25 +0900

    Commons log add, Spring Dependency add

--- src/main/scala/com/example/TestScala.scala
+++ src/main/scala/com/example/TestScala.scala
@@ -1,9 +1,22 @@
 package com.example
 
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
+
+
+
 
 
 object TestScala {
+	
+	var log = LogFactory.getLog(TestScala.getClass.getName)
+	
 	def main(args: Array[String]): Unit = {
 		println("Scala Execution Test...");
+		
+		         log.info("############################")
+                log.info("TestScala Commons logging...");
+                log.info("############################")
+
 	}
 }
